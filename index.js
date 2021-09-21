6933bf8e27542a78709ba9c98201e4ccc23a96b4
Lisa <redlisa125@gmail.com>
2021-09-21 07:10:20 -1000

    commit tree 5c58edaabff25edbd3d51656e95f92a1e179e606

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flare = "*") {
+    return function (adjective = "special") {
+        return `You are ${flare}${adjective}${flare}!`;
+    };
+}
+
+wrapAdjective("a hard worker")()
